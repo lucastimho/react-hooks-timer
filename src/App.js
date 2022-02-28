3a7ba9ffa60ca48ff0787d5408cdf9680f14321c
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-28 13:48:24 -0600

    adds days, hours, minutes, and seconds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
+const caculateTimeLeft = () => {
+  let year = new Date().getFullYear();
+  const difference = +new Date(`10/01/${year}`) - +new Date();
+  let timeLeft = {};
 
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor(((difference / 1000) * 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+  return timeLeft;
+};
 function App() {
   return <div></div>;
 }
