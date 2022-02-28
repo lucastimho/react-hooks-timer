caf9fe684f730fda37cbcdee6513ff84bf2d72db
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-28 14:00:12 -0600

    edits imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 const caculateTimeLeft = () => {
   let year = new Date().getFullYear();
